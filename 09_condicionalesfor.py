d1700747f831300a2cbf373ef59dfc3fec345664
Minerva-HG <minerva.hernandez@megacable.com.mx>
2023-02-08 17:24:58 -0600

    Modificaciones 1

---
+++ 09_condicionalesfor.py
@@ -0,0 +1,8 @@
+#condicionales con For
+lenguajes= ['Python','Kotlin','Java','JavaScript','Ruby']
+
+for lenguaje in lenguajes:
+    if lenguaje == 'Python':
+        print(lenguaje.upper())
+    else:
+        print(lenguaje)
